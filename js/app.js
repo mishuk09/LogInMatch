7172a6fc3b2e6edd9b16299f19cb5cbaef63e55b
mishuk09 <mishukinfo09@gmail.com>
2023-06-26 05:20:35 -0400

    JS

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+
+    } else {
+        return getPin;
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    console.log(pin);
+
+
+}
