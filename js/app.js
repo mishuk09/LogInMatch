90996ccb332f4a4d77ffaf6399afa86e4287e46c
mishuk09 <mishukinfo09@gmail.com>
2023-06-26 06:09:27 -0400

    JS

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ function getPin() {
     const pinString = pin + '';
     if (pinString.length == 4) {
         return pin;
-
     } else {
         return getPin;
     }
@@ -12,6 +11,43 @@ function getPin() {
 function generatePin() {
     const pin = getPin();
     console.log(pin);
+    document.getElementById('display-pin').value = pin;
 
+}
+document.getElementById('key-pad').addEventListener('click', function(event) {
+    const number = event.target.innerText;
+    const calc = document.getElementById('typed-numbers');
+
+
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calc.value = '';
+        }
+
+    } else {
+
+        const previousNumber = calc.value;
+        const finalNumber = previousNumber + number;
+        calc.value = finalNumber;
+
+    }
+});
+
+function verifyPin() {
+    const randompin = document.getElementById('display-pin').value;
+    const typeNumber = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-faild');
+
+
+    if (randompin == typeNumber) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+
+    } else {
+        failError.style.display = 'block';
+        successMessage.style.display = 'none';
+
+    }
 
 }
